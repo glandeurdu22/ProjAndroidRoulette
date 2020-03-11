16c54c4af48216d9b8acb3715d5f183b5548efe3
Lionel <Lionel.darcy.28.12.96@gmail.com>
2020-03-11 18:45:05 +0100

    connexion et enregistrement

--- app/src/main/java/com/example/applicationroulette/MainActivity.java
+++ app/src/main/java/com/example/applicationroulette/MainActivity.java
@@ -1,30 +1,141 @@
 package com.example.applicationroulette;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button ConnexionId, CreerCompteId;
+    EditText EmailId, PasswordId;
+    private static String LOGIN_URL = "https://randojoe.000webhostapp.com/login.php";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        EmailId = findViewById(R.id.txtEmail);
+        PasswordId = findViewById(R.id.txtPasswordLogin);
+        ConnexionId = findViewById(R.id.btnConnexion);
+        CreerCompteId = findViewById(R.id.btnCreerUnCompte);
+        ecouteConnexion();
+        ecouteEnregistrement();
+
+
+
     }
+
+    private void ecouteConnexion() {
+        ConnexionId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = EmailId.getText().toString().trim();
+                String password = PasswordId.getText().toString().trim();
+                //on vérifie que l'email et le mot de passe sont rentrés
+                if (email.isEmpty()) {
+                    EmailId.setError("Veuillez saisir un identifiant");
+                    EmailId.requestFocus();
+                } else if (password.isEmpty()) {
+                    PasswordId.setError("Veuillez saisir un mot de passe");
+                    PasswordId.requestFocus();
+
+                    //on vérifie si aucun champs n'est remplis
+                } else {
+                   Login(email,password);
+
+                }
+
+
+            }
+        });
+
+    }
+
+    private void Login(final String email, final String password) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, LOGIN_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("login");
+
+                            if(success.equals("1")){
+                                for (int i = 0; i < jsonArray.length(); i++){
+                                    JSONObject object = jsonArray.getJSONObject(i);
+                                    String name = object.getString("name").trim();
+                                    String email = object.getString("email").trim();
+
+                                    startActivity(new Intent(MainActivity.this,RegistrationActivity.class));
+
+                                    Toast.makeText(MainActivity.this,"Vous êtes connecté. \nVotre nom est: "+ name+" Votre email est "+email, Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                            Toast.makeText(MainActivity.this,"la connexion a échoué "+e.toString(), Toast.LENGTH_SHORT).show();
+                           // loading.setVisibility(View.GONE);
+                            //btnEnregistrerId.setVisibility(View.VISIBLE);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,"La connexion a échoué "+error.toString(), Toast.LENGTH_SHORT).show();
+                        //loading.setVisibility(View.GONE);
+                       // btnEnregistrerId.setVisibility(View.VISIBLE);
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError{
+                Map<String, String> params = new HashMap<>();
+                params.put("email",email);
+                params.put("password",password);
+                return params;
+            }
+        };
+    }
+
+    //Ecoute évènement sur bouton connexion
+    private void ecouteEnregistrement(){
+        CreerCompteId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,RegistrationActivity.class));
+
+                Toast.makeText(MainActivity.this,"Enregistrer un compte",Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
 
 }
---
+++ app/src/main/java/com/example/applicationroulette/RegistrationActivity.java
@@ -0,0 +1,131 @@
+package com.example.applicationroulette;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    EditText PrenomId;
+    EditText EmailId;
+    //EditText AdresseId;
+    //EditText TelephoneId;
+    EditText PasswordId;
+    Button btnEnregistrerId;
+   // private ProgressBar loading;
+    private static String REGISTER_URL = "https://randojoe.000webhostapp.com/register.php";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        PrenomId = findViewById(R.id.txtPrenom);
+        EmailId = findViewById(R.id.txtEmailEnre);
+        //AdresseId = findViewById(R.id.txtAdresse);
+        //TelephoneId = findViewById(R.id.txtTelephone);
+        PasswordId = findViewById(R.id.txtPasswordEnregistrer);
+        btnEnregistrerId = findViewById(R.id.btnEnregistrer);
+        EnregistrerAction();
+    }
+
+    public void EnregistrerAction (){
+
+        btnEnregistrerId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EnregistrerMembre();
+
+            }
+        });
+
+    }
+
+
+
+    private void EnregistrerMembre() {
+        //loading.setVisibility(View.VISIBLE);
+      //  btnEnregistrerId.setVisibility(View.GONE);
+
+    //Récupération des saisis utilisateur
+       final String name = PrenomId.getText().toString();
+       final String email = EmailId.getText().toString();
+       //final String adresse = AdresseId.getText().toString();
+       //final String telephone = TelephoneId.getText().toString();
+       final String password = PasswordId.getText().toString();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+
+                            if(success.equals("1")){
+                                Toast.makeText(RegistrationActivity.this,"Vous êtes enregistré", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                            Toast.makeText(RegistrationActivity.this,"L'enregistrement a échoué "+e.toString(), Toast.LENGTH_LONG).show();
+                            //loading.setVisibility(View.GONE);
+                           // btnEnregistrerId.setVisibility(View.VISIBLE);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(RegistrationActivity.this,"L'enregistrement a échoué 2 "+error.toString(), Toast.LENGTH_SHORT).show();
+                       // loading.setVisibility(View.GONE);
+                        //btnEnregistrerId.setVisibility(View.VISIBLE);
+                    }
+                })
+
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("name",name);
+                params.put("email",email);
+                params.put("password",password);
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+
+    }
+
+
+}
